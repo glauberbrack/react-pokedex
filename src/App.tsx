7b781d8bf8fbbf8eff42c0be57a10b26a77a446c
glauberbrack <glauber@brack.com.br>
2020-12-17 11:25:21 -0300

    chore: initliaze project and eslint configuration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Routes from './routes';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div>teste</div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
+const Routes: React.FC = () => {
+  return <BrowserRouter />;
+};
+
+export default Routes;
